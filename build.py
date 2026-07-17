ab85ecff8b2b911eb78603885aed48bc90d39cd3
irinapeyrot <irina.peyrot@davinci.edu.ar>
2026-07-17 19:25:09 -0300

    Delete build.py

--- build.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-tpl = open('index.template.html', encoding='utf-8').read()
-svg = open('map.svg', encoding='utf-8').read()
-out = tpl.replace('<!--MAP_SVG-->', svg)
-open('index.html', 'w', encoding='utf-8').write(out)
-print('index.html', len(out), 'bytes')
